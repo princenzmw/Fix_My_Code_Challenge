d519af1eebf7b4fcdc9e81b0047315388bb4c57e
princenzmw <princenzmw@gmail.com>
2024-02-26 03:16:13 +0200

    I do not know what is going on here...!

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,43 +2,52 @@
 #include <stdlib.h>
 
 /**
-* delete_dnodeint_at_index - Delete a node at a specific index from a list
-*
-* @head: A pointer to the first element of a list
-* @index: The index of the node to delete
-*
-* Return: 1 on success, -1 on failure
-*/
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = *head;
-	unsigned int i;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (*head == NULL)
-		return (-1);
-
-	if (index == 0)
 	{
-		*head = current->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(current);
-		return (1);
-	}
-
-	for (i = 0; current != NULL && i < index; i++)
-	{
-		current = current->next;
-	}
-
-	if (current == NULL)
 		return (-1);
-
-	current->prev->next = current->next;
-	if (current->next != NULL)
-		current->next->prev = current->prev;
-
-	free(current);
-
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
 	return (1);
 }
